675a46c25bba639abb769cd69fbbbf39fc7dac0a
inesdoreau <idoro@hotmail.fr>
2026-09-02 12:14:36 +0200

    Setup Multiplayer
    Add Keykit packages

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,36 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+
+public class GameManager : NetworkBehaviour
+{
+    [SerializeField] private MultiplayerUI m_multiplayerUI;
+
+    private void Start()
+    {
+        if(m_multiplayerUI != null)
+        {
+            m_multiplayerUI.OnStartHost += StartHost;
+            m_multiplayerUI.OnStartClient += StartClient;
+            m_multiplayerUI.OnDiconnectClient += DisconnectClient;
+        }
+    }
+
+    private void DisconnectClient()
+    {
+        m_multiplayerUI.DisableButtons();
+        NetworkManager.Shutdown();
+    }
+
+    private void StartClient()
+    {
+        m_multiplayerUI.DisableButtons();
+        NetworkManager.StartClient();
+    }
+
+    private void StartHost()
+    {
+       m_multiplayerUI.DisableButtons();
+        NetworkManager.StartHost();
+    }
+}
